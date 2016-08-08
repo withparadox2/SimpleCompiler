6294e58d038cd4dd36fd48f5b7f23618d02886b6
withparadox2 <withparadox2@gmail.com>
2016-08-08 23:08:25 +0800

    Clear readme.

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by withparadox2 on 2016/8/8.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by withparadox2 on 2016/8/8.
+//
+
+#ifndef TEST_MAIN_H
+#define TEST_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //TEST_MAIN_H
