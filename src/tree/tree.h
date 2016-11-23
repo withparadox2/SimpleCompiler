1e45aac0226ab13e924f5dbb149a7b5edbbfa722
withparadox2 <withparadox2@gmail.com>
2016-11-23 22:25:33 +0800

    Tags

--- src/tree/tree.h
+++ src/tree/tree.h
@@ -16,14 +16,130 @@ public:
 
     //Type* type;
     virtual void accept(Visitor &v) = 0;
-    template <typename R, typename D>
-    R accept(TreeVisitor<R, D> v, D& d);
+
+    template<typename R, typename D>
+    R accept(TreeVisitor<R, D> v, D &d);
+
+    static const int CLASSDEF = 1;
+
+    /** Method definitions, of type MethodDef.
+     */
+    static const int METHODDEF = CLASSDEF + 1;
+
+    /** Variable definitions, of type VarDef.
+     */
+    static const int VARDEF = METHODDEF + 1;
+
+    /** The no-op statement ";", of type Skip
+     */
+    static const int SKIP = VARDEF + 1;
+
+    /** Blocks, of type Block.
+     */
+    static const int BLOCK = SKIP + 1;
+
+    /** For-loops, of type ForLoop.
+     */
+    static const int FORLOOP = SKIP + 1;
+
+    /** Conditional statements, of type If.
+     */
+    static const int IF = FORLOOP + 1;
+
+    /** Expression statements, of type Exec.
+     */
+    static const int EXEC = IF + 1;
+
+    /** Break statements, of type Break.
+     */
+    static const int BREAK = EXEC + 1;
+
+    /** Continue statements, of type Continue.
+     */
+    static const int CONTINUE = BREAK + 1;
+
+    /** Return statements, of type Return.
+     */
+    static const int RETURN = CONTINUE + 1;
+
+    /** Method invocation expressions, of type Apply.
+     */
+    static const int APPLY = ASSERT + 1;
+
+    /** Class instance creation expressions, of type NewClass.
+     */
+    static const int NEWCLASS = APPLY + 1;
+
+    /** Array creation expressions, of type NewArray.
+     */
+    static const int NEWARRAY = NEWCLASS + 1;
+
+    /** Parenthesized subexpressions, of type Parens.
+     */
+    static const int PARENS = NEWARRAY + 1;
+
+    /** Assignment expressions, of type Assign.
+     */
+    static const int ASSIGN = PARENS + 1;
+
+    /** Indexed array expressions, of type Indexed.
+     */
+    static const int INDEXED = TYPETEST + 1;
+
+    /** Selections, of type Select.
+     */
+    static const int SELECT = INDEXED + 1;
+
+    /** Simple identifiers, of type Ident.
+     */
+    static const int IDENT = SELECT + 1;
+
+    /** Literals, of type Literal.
+     */
+    static const int LITERAL = IDENT + 1;
+
+    /** Basic type identifiers, of type TypeIdent.
+     */
+    static const int TYPEIDENT = LITERAL + 1;
+
+    /** Array types, of type TypeArray.
+     */
+    static const int TYPEARRAY = TYPEIDENT + 1;
+    /** metadata: Modifiers
+     */
+    static const int MODIFIERS = TYPEIDENT + 1;
+
+    /** Unary operators, of type Unary.
+     */
+    static const int POS = MODIFIERS + 1;             // +
+    static const int NEG = POS + 1;                   // -
+    static const int NOT = NEG + 1;                   // !
+    static const int COMPL = NOT + 1;                 // ~
+
+
+    /** unary operator for null reference checks, only used internally.
+     */
+    static const int NULLCHK = POSTDEC + 1;
+
+    /** Binary operators, of type Binary.
+     */
+    static const int OR = NULLCHK + 1;                // ||
+    static const int AND = OR + 1;                    // &&
+    static const int EQ = AND + 1;                 // ==
+    static const int NE = EQ + 1;                     // !=
+    static const int LT = NE + 1;                     // <
+    static const int GT = LT + 1;                     // >
+    static const int PLUS = GT + 1;                  // +
+    static const int MINUS = PLUS + 1;                // -
+    static const int MUL = MINUS + 1;                 // *
+    static const int DIV = MUL + 1;                   // /
+    static const int MOD = DIV + 1;                   // %
 };
 
 class JCClassDecl : public Tree {
 public:
     Name &name;
-    vector<JCTree*> defs;
+    vector<JCTree *> defs;
 //    ClassSymbol sym;
 };
 
