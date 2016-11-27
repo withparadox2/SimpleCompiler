b35f421f8da676df973e5cf7842da832bd60d90c
withparadox2 <withparadox2@gmail.com>
2016-11-27 21:24:57 +0800

    Assign ID to tokens.

--- tools/formatToken.js
+++ tools/formatToken.js
@@ -1,97 +1,103 @@
 var text = `
-    static Token ERROR;
-    static Token _EOF;
+Token Token::_EOF;
+Token Token::ERROR;
+Token Token::IDENTIFIER;
+Token Token::BOOLEAN("boolean");
+Token Token::CLASS("class");
+Token Token::ELSE("else");
+Token Token::FOR("for");
+Token Token::IF("if");
+Token Token::INT("int");
+Token Token::NEW("new");
+Token Token::PRIVATE("private");
+Token Token::PUBLIC("public");
+Token Token::RETURN("return");
+Token Token::STATIC("static");
+Token Token::THIS("this");
+Token Token::VOID("void");
 
-    static Token IDENTIFIER;
-    static Token BOOLEAN;
-    static Token CLASS;
-    static Token ELSE;
-    static Token FOR;
-    static Token IF;
-    static Token INT;
-    static Token NEW;
-    static Token PRIVATE;
-    static Token PUBLIC;
-    static Token RETURN;
-    static Token STATIC;
-    static Token THIS;
-    static Token VOID;
+Token Token::STRINGLITERAL;
 
-    static Token STRINGLITERAL;
-    static Token INTLITERAL;
-    static Token CHARLITERAL;
-    static Token TRUE;
-    static Token FALSE;
+Token Token::INTLITERAL;
 
-    static Token NULL_;
-    static Token LPAREN;
-    static Token RPAREN;
-    static Token LBRACE;
-    static Token RBRACE;
-    static Token LBRACKET;
-    static Token RBRACKET;
+Token Token::CHARLITERAL;
 
-    static Token SEMI;
-    static Token COMMA;
-    static Token DOT;
-    static Token ELLIPSIS;
-    static Token EQ;
-    static Token GT;
-    static Token LT;
-    static Token BANG;
-    static Token TILDE;
-    static Token QUES;
-    static Token COLON;
-    static Token EQEQ;
-    static Token LTEQ;
-    static Token GTEQ;
-    static Token BANGEQ;
-    static Token AMPAMP;
-    static Token BARBAR;
-    static Token PLUSPLUS;
-    static Token SUBSUB;
-    static Token PLUS;
-    static Token SUB;
-    static Token STAR;
-    static Token SLASH;
-    static Token AMP;
-    static Token BAR;
-    static Token CARET;
-    static Token PERCENT;
-    static Token LTLT;
-    static Token GTGT;
-    static Token GTGTGT;
-    static Token PLUSEQ;
-    static Token SUBEQ;
-    static Token STAREQ;
-    static Token SLASHEQ;
-    static Token AMPEQ;
-    static Token BAREQ;
-    static Token CARETEQ;
-    static Token PERCENTEQ;
-    static Token LTLTEQ;
-    static Token GTGTEQ;
-    static Token GTGTGTEQ;
-    static Token MONKEYS_AT;
+Token Token::TRUE("true");
+Token Token::FALSE("false");
+
+Token Token::NULL_("null");
+Token Token::LPAREN("(");
+Token Token::RPAREN(")");
+Token Token::LBRACE("{");
+Token Token::RBRACE("}");
+Token Token::LBRACKET("[");
+Token Token::RBRACKET("]");
+
+Token Token::SEMI(";");
+Token Token::COMMA(",");
+Token Token::DOT(".");
+Token Token::ELLIPSIS("...");
+Token Token::EQ("=");
+Token Token::GT(">");
+Token Token::LT("<");
+Token Token::BANG("!");
+Token Token::TILDE("~");
+Token Token::QUES("?");
+Token Token::COLON(":");
+Token Token::EQEQ("==");
+Token Token::LTEQ("<=");
+Token Token::GTEQ(">=");
+Token Token::BANGEQ("!=");
+Token Token::AMPAMP("&&");
+Token Token::BARBAR("||");
+Token Token::PLUSPLUS("++");
+Token Token::SUBSUB("--");
+Token Token::PLUS("+");
+Token Token::SUB("-");
+Token Token::STAR("*");
+Token Token::SLASH("/");
+Token Token::AMP("&");
+Token Token::BAR("|");
+Token Token::CARET("^");
+Token Token::PERCENT("%");
+Token Token::LTLT("<<");
+Token Token::GTGT(">>");
+Token Token::GTGTGT(">>>");
+Token Token::PLUSEQ("+=");
+Token Token::SUBEQ("-=");
+Token Token::STAREQ("*=");
+Token Token::SLASHEQ("/=");
+Token Token::AMPEQ("&=");
+Token Token::BAREQ("|=");
+Token Token::CARETEQ("^=");
+Token Token::PERCENTEQ("%=");
+Token Token::LTLTEQ("<<=");
+Token Token::GTGTEQ(">>=");
+Token Token::GTGTGTEQ(">>>=");
+Token Token::MONKEYS_AT("@");
 `
 
 var array = text.split('\n');
 
-var preName = '';
-var ID_REGEX = /(ID.*?)=.*;/;
+function replacer(match, p1, p2, offset, string) {
+    if (p2) {
+        return `::${p1}(ID_${p1}, ${p2});`;
+    } else {
+        return `::${p1}(ID_${p1});`;
+    }
+}
+
+var test = /::(\w*)(?:\((.*)\))?;/
+
 for (var i = 0; i < array.length; i++) {
+
     var item = array[i];
     if (item.trim().length == 0) {
         continue;
     }
-    item = item.replace('static Token ', 'const static int ID_');
 
-    item = item.replace(';', ` = ${preName} + 1;`);
-    array[i] = item;
 
-    console.log(item)
-
-    preName = item.match(ID_REGEX)[1];
+    array[i] = item.replace(test, replacer);
 }
 
 var fs = require('fs');
